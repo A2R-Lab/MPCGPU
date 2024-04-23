4e8440cceb992a315d8891f7894d1075d1094baf
Brennan Xavier McManus <bm2530@columbia.edu>
2024-04-23 18:35:37 -0400

    Set up vectors for results instead of individual variables

--- include/mpcsim_n.cuh
+++ include/mpcsim_n.cuh
@@ -54,12 +54,12 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
     
     
     // struct timespec solve_start, solve_end;
-    double sqp_solve_time_us = 0;               // current sqp solve time
-    double simulation_time = 0;                 // current simulation time
-    double prev_simulation_time = 0;            // last simulation time
-    double time_since_timestep = 0;             // time since last timestep of original trajectory
-    bool shifted = false;                       // has xu been shifted
-    uint32_t traj_offset = 0;                        // current goal states of original trajectory
+    std::vector<double> sqp_solve_time_us_vec(solve_count, 0);               // current sqp solve time
+    std::vector<double> simulation_time_vec(solve_count, 0);                 // current simulation time
+    std::vector<double> prev_simulation_time_vec(solve_count, 0);            // last simulation time
+    std::vector<double> time_since_timestep_vec(solve_count, 0);             // time since last timestep of original trajectory
+    std::vector<bool> shifted_vec(solve_count, false);                       // has xu been shifted
+    std::vector<uint32_t> traj_offset_vec(solve_count, 0);                        // current goal states of original trajectory
 
 
     // vars for recording data
@@ -162,10 +162,11 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
     for(control_update_step_vec[0] = 0; control_update_step_vec[0] < max_control_updates; control_update_step_vec[0]++){
         
 
-        if (traj_offset == traj_steps){ break; }
+        if (traj_offset_vec[0] == traj_steps){ break; }
 
 
-
+// don't need to change this for now, it's just doing the 0th problem but
+// TODO update this to print all $solve_count paths
 #if LIVE_PRINT_PATH
         grid::end_effector_positions_kernel<T><<<1,128>>>(d_eePos, d_xs, grid::NUM_JOINTS, (grid::robotModel<T> *) d_dynmem, 1);
         gpuErrchk(cudaMemcpy(h_eePos, d_eePos, 6*sizeof(T), cudaMemcpyDeviceToHost));
@@ -184,30 +185,30 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
 
         cur_linsys_iters_vec[0] = std::get<0>(sqp_stats_vec[0]);
         cur_linsys_times_vec[0] = std::get<1>(sqp_stats_vec[0]);
-        sqp_solve_time_us = std::get<2>(sqp_stats_vec[0]);
+        sqp_solve_time_us_vec[0] = std::get<2>(sqp_stats_vec[0]);
         cur_sqp_iters_vec[0] = std::get<3>(sqp_stats_vec[0]);
         sqp_exits_vec[0].push_back(std::get<4>(sqp_stats_vec[0]));
         cur_linsys_exits_vec[0] = std::get<5>(sqp_stats_vec[0]);
 
 
 #if CONST_UPDATE_FREQ
-        simulation_time = SIMULATION_PERIOD;
+        simulation_time_vec[0] = SIMULATION_PERIOD;
 #else
-        simulation_time = sqp_solve_time_us;
+        simulation_time_vec[0] = sqp_solve_time_us_vec[0];
 #endif
         
 
         // simulate traj for current solve time, offset by previous solve time
-        simple_simulate<T>(state_size, control_size, knot_points, d_xs, d_xu_old, d_dynmem, timestep, prev_simulation_time, simulation_time);
+        simple_simulate<T>(state_size, control_size, knot_points, d_xs, d_xu_old, d_dynmem, timestep, prev_simulation_time_vec[0], simulation_time_vec[0]);
 
         // old xu = new xu
         gpuErrchk(cudaMemcpy(d_xu_old, d_xu, traj_len*sizeof(T), cudaMemcpyDeviceToDevice));
 
 
-        time_since_timestep += simulation_time * 1e-6;
+        time_since_timestep_vec[0] += simulation_time_vec[0] * 1e-6;
 
         // if shift_threshold% through timestep
-        if (!shifted && time_since_timestep > shift_threshold){
+        if (!shifted_vec[0] && time_since_timestep_vec[0] > shift_threshold){
             
             // record tracking error
             grid::end_effector_positions_kernel<T><<<1,128>>>(d_eePos, d_xs, grid::NUM_JOINTS, (grid::robotModel<T> *) d_dynmem, 1);
@@ -220,13 +221,13 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
             // std::cout << cur_tracking_error_vec[0] << std::endl;;
             tracking_errors_vec[0].push_back(cur_tracking_error_vec[0]);
             
-            traj_offset++;
+            traj_offset_vec[0]++;
 
             // shift xu
             just_shift<T>(state_size, control_size, knot_points, d_xu);             // shift everything over one
-            if (traj_offset + knot_points < traj_steps){
+            if (traj_offset_vec[0] + knot_points < traj_steps){
                 // if within precomputed traj, fill in last state, control with precompute
-                gpuErrchk(cudaMemcpy(&d_xu[traj_len - (state_size + control_size)], &d_xu_traj[(state_size+control_size)*traj_offset - control_size], sizeof(T)*(state_size+control_size), cudaMemcpyDeviceToDevice));     // last state filled from precomputed trajectory
+                gpuErrchk(cudaMemcpy(&d_xu[traj_len - (state_size + control_size)], &d_xu_traj[(state_size+control_size)*traj_offset_vec[0] - control_size], sizeof(T)*(state_size+control_size), cudaMemcpyDeviceToDevice));     // last state filled from precomputed trajectory
             }
             else{
                 // fill in last state with goal position, zero velocity, last control with zero control
@@ -237,8 +238,8 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
             
             // shift goal
             just_shift(6, 0, knot_points, d_eePos_goal);
-            if (traj_offset + knot_points < traj_steps){
-                gpuErrchk(cudaMemcpy(&d_eePos_goal[(knot_points-1)*(6)], &d_eePos_traj[(traj_offset+knot_points-1) * (6)], 6*sizeof(T), cudaMemcpyDeviceToDevice));
+            if (traj_offset_vec[0] + knot_points < traj_steps){
+                gpuErrchk(cudaMemcpy(&d_eePos_goal[(knot_points-1)*(6)], &d_eePos_traj[(traj_offset_vec[0]+knot_points-1) * (6)], 6*sizeof(T), cudaMemcpyDeviceToDevice));
             }
             else{
                 // fill in last goal state with goal state and zero velocity
@@ -250,19 +251,19 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
             just_shift(state_size, 0, knot_points, d_lambda);
                 // gpuErrchk(cudaMemset(&lambdas[i][state_size*(knot_points-1)], 0, state_size*sizeof(T)));
             
-            shifted = true;
+            shifted_vec[0] = true;
         }
 
-        if (time_since_timestep > timestep){
-            // std::cout << "shifted to offset: " << traj_offset + 1 << std::endl;
-            shifted = false;
-            time_since_timestep = std::fmod(time_since_timestep, timestep);
+        if (time_since_timestep_vec[0] > timestep){
+            // std::cout << "shifted to offset: " << traj_offset_vec[0] + 1 << std::endl;
+            shifted_vec[0] = false;
+            time_since_timestep_vec[0] = std::fmod(time_since_timestep_vec[0], timestep);
         }
         gpuErrchk(cudaMemcpy(d_xu, d_xs, state_size*sizeof(T), cudaMemcpyDeviceToDevice));
 
 
         
-        prev_simulation_time = simulation_time;
+        prev_simulation_time_vec[0] = simulation_time_vec[0];
 
         gpuErrchk(cudaPeekAtLastError());
 
@@ -273,7 +274,7 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
         linsys_exits_vec[0].insert(linsys_exits_vec[0].end(), cur_linsys_exits_vec[0].begin(), cur_linsys_exits_vec[0].end());
         gpuErrchk(cudaMemcpy(h_xs, d_xs, state_size*sizeof(T), cudaMemcpyDeviceToHost));
         tracking_path_vec[0].push_back(std::vector<T>(h_xs, &h_xs[state_size]));                                   // next state
-        sqp_times_vec[0].push_back(sqp_solve_time_us);
+        sqp_times_vec[0].push_back(sqp_solve_time_us_vec[0]);
         sqp_iters_vec[0].push_back(cur_sqp_iters_vec[0]);
 
 
@@ -286,7 +287,7 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
             std::cout << "linear system solve time:" << std::endl;
             printStats<double>(&linsys_times_vec[0]);
     #endif // #if TIME_LINSYS
-            std::cout << "goal offset [" << traj_offset << "]\n";
+            std::cout << "goal offset [" << traj_offset_vec[0] << "]\n";
             std::cout << "sqp iters" << std::endl;
             printStats<uint32_t>(&sqp_iters_vec[0]);
             std::cout << "sqp times" << std::endl;
@@ -299,7 +300,7 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
                std::cout << "WARNING: PCG exiting for max iter over 50% of the time" << std::endl;
             }
             
-            std::cout << "avg tracking error: " << std::accumulate(tracking_errors_vec[0].begin(), tracking_errors_vec[0].end(), 0.0f) / traj_offset << " current error: " << cur_tracking_error_vec[0] << "\n";
+            std::cout << "avg tracking error: " << std::accumulate(tracking_errors_vec[0].begin(), tracking_errors_vec[0].end(), 0.0f) / traj_offset_vec[0] << " current error: " << cur_tracking_error_vec[0] << "\n";
             std::cout << std::endl;
 
         }
@@ -310,7 +311,7 @@ std::tuple<std::vector<toplevel_return_type>, std::vector<linsys_t>, linsys_t> s
     }
 #if SAVE_DATA
     dump_tracking_data(&linsys_iters_vec[0], &linsys_exits_vec[0], &linsys_times_vec[0], &sqp_times_vec[0], &sqp_iters_vec[0], &sqp_exits, &tracking_errors_vec[0], &tracking_path_vec[0], 
-            traj_offset, control_update_step_vec[0], start_state_ind, goal_state_ind, test_iter, test_output_prefix);
+            traj_offset_vec[0], control_update_step_vec[0], start_state_ind, goal_state_ind, test_iter, test_output_prefix);
 #endif
     
 
