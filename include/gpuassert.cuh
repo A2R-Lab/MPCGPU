4b50f68ef198150e598a3f1b5e0ca87dc7c7e777
plancher <plancherb1@gmail.com>
2026-06-27 00:07:59 -0400

    Guard gpuassert.cuh with #ifndef gpuErrchk (defer to consumer's grid.cuh; avoids ODR clash)

--- include/gpuassert.cuh
+++ include/gpuassert.cuh
@@ -1,10 +1,13 @@
 #pragma once
 #include <stdio.h>
 
-
+// Defer to the consumer's gpuErrchk/gpuAssert if one is already defined (e.g. GRiD's
+// grid.cuh defines both unconditionally). Guarding on the gpuErrchk macro avoids an
+// ODR clash when a TU includes both this header and grid.cuh.
+#ifndef gpuErrchk
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
-   if (code != cudaSuccess) 
+   if (code != cudaSuccess)
    {
       fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
       if (abort) exit(code);
@@ -12,3 +15,4 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 }
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+#endif
